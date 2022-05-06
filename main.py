3068a86f01227212e260a4f1b3f29a9cb112f094
Dmitry Korolev <dikorolyov@yandex-team.ru>
2022-05-06 22:21:17 +0300

    add pseudocode

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+
+from telethon import TelegramClient
+
+
+def test():
+    with TelegramClient('anon', os.environ.get('API_ID'), os.environ.get(
+            'API_HASH')) as client:  # TODO вынести получение этих переменных в отдельные функции
+        client.loop.run_until_complete(client.send_message('me', 'Pivo'))
+
+
 def main():
-    pass
+    send_reports_to_main_account()
+    send_to_chat_bot('Здарова')
+    wait_chat_bot_to_send('Привет, кидай задачи')
+    for i in []:  # список ников пользователей
+        forward_last_message_from(i)
+        wait_chat_bot_to_send('Норм, ебашь ещё')
+    test()
 
 
 if __name__ == '__main__':
