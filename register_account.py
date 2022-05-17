e8b18f59fa9675feeb04299db85e05f34bd0211e
Dmitry Korolev <dikorolyov@yandex-team.ru>
2022-05-17 22:14:17 +0300

    register account script added

---
+++ register_account.py
@@ -0,0 +1,13 @@
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+import settings
+
+
+def register_account():
+    with TelegramClient(StringSession(), settings.API_ID, settings.API_HASH) as client:
+        print(client.session.save())
+
+
+if __name__ == '__main__':
+    register_account()
