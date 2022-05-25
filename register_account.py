4537092f7efb27d0ea4386fd11f1b5c92966ab27
Dmitry Korolev <dikorolyov@yandex-team.ru>
2022-05-25 23:47:30 +0300

    add proxy for acc registration

--- register_account.py
+++ register_account.py
@@ -1,11 +1,21 @@
+import socks
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 import settings
 
+socks.set_default_proxy(
+    socks.SOCKS5,
+    settings.PROXY_HOST,
+    settings.PROXY_PORT,
+    settings.PROXY_LOGIN,
+    settings.PROXY_PASSWORD,
+)
+proxy = socks.get_default_proxy()
+
 
 def register_account():
-    with TelegramClient(StringSession(), settings.API_ID, settings.API_HASH) as client:
+    with TelegramClient(StringSession(), settings.API_ID, settings.API_HASH, proxy=proxy) as client:
         print(client.session.save())
 
 
